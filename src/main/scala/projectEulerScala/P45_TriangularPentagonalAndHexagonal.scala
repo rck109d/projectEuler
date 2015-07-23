26887c4e792f3aec5fe822a78d938ee6625b51bb
rck109d <rck109d@gmail.com>
2015-07-23 10:22:41 -0400

    added P45, P46

---
+++ src/main/scala/projectEulerScala/P45_TriangularPentagonalAndHexagonal.scala
@@ -0,0 +1,20 @@
+package main.scala.projectEulerScala
+
+object P45_TriangularPentagonalAndHexagonal {
+  def pentagonal = Stream.from(1).map(n => n.toLong * (3 * n - 1) / 2)
+
+  def hexagonal = Stream.from(1).map(n => n.toLong * (2 * n - 1))
+
+  def intersect(a: Stream[Long], b: Stream[Long]): Stream[Long] = (a, b) match {
+    case (ahead #:: as, bhead #:: bs) =>
+      if (ahead < bhead) intersect(as, b)
+      else if (ahead > bhead) intersect(a, bs)
+      else ahead #:: intersect(as, bs)
+  }
+
+
+  def main(args: Array[String]) {
+    val joined = intersect(pentagonal, hexagonal)
+    println(joined.dropWhile(_ <= 40755).head)
+  }
+}
